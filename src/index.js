e23e85e1210f780fef5a30b0f95c927f4f20090b
Daniil Yuzhanin <daniilyuzhanin@gmail.com>
2020-10-25 21:50:01 +0300

    feat: end morse

--- src/index.js
+++ src/index.js
@@ -38,9 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	 // write your solution here
+	let arr = [];
+
+	for (i = 0; i < expr.length; i += 10) {
+		let a = '';
+	
+		for (let k = 0; k < 10; k += 2) {
+			if (expr[k + i] + expr[k + i + 1] === '**') {
+				a += ' ';
+			}
+			if (expr[k + i] + expr[k + i + 1] === '10') {
+				a += '.';
+			}
+			if (expr[k + i] + expr[k + i + 1] === '11') {
+				a += '-'
+			}
+		}
+		arr.push(a)
+	}
+	return arr.map((item) => (MORSE_TABLE[item] ? MORSE_TABLE[item] : " ")).join("");
 }
 
 module.exports = {
     decode
-}
+};
